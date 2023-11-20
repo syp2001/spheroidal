7ad6d959d4a4af60f2b22d99f8c3f2bd43bb08fb
Seyong Park <seyongpark2001@gmail.com>
2023-11-20 15:39:19 -0500

    leaver harmonic

--- spheroidal/spheroidal.py
+++ spheroidal/spheroidal.py
@@ -112,16 +112,83 @@ def eigenvalue_leaver(s,ell,m,g):
     # compute eigenvalue using root finding with newton's method
     return root_scalar(continued_fraction,args=(s,ell,m,g),x0=spectral_A,fprime=continued_fraction_deriv,method="newton").root+g**2-2*m*g
 
-
-    :return: spin-weighted spheroidal harmonic :math:`{}_{s}S_{lm}`
-    :rtype: function
+def leaver_coefficients(s,ell,m,g,num_terms=None,n_max=100):
     """
-    l_min = max(abs(s),abs(m))
-    coefficients = coupling_coefficients(s,ell,m,g,num_terms)
+    Computes the coefficients of the Frobenius expansion in equation 18 of `(Leaver, 1985) <https://www.edleaver.com/Misc/EdLeaver/Publications/AnalyticRepresentationForQuasinormalModesOfKerrBlackHoles.pdf>`_
+
+    :param s: spin weight
+    :type s: half-integer
+    :param ell: degree
+    :type ell: half-integer
+    :param m: order
+    :type m: half-integer
+    :param g: spheroicity
+    :type g: double
+    :param num_terms: number of coefficients to compute, automatic by default
+    :type num_terms: int, optional
+    :param n_max: maximum number of coefficients to compute, defaults to 100
+    :type n_max: int, optional
+
+    :return: normalized array of coefficients
+    :rtype: numpy.ndarray
+    """
+    A = np.real(eigenvalue_leaver(s,ell,m,g)-g**2+2*m*g) # angular separation constant
+    
+    k1 = 1/2*abs(m-s)
+    k2 = 1/2*abs(m+s)
+    
+    alpha = lambda n: -2*(n+1)*(n+2*k1+1)
+    beta = lambda n,A: n*(n - 1) + 2*n*(k1 + k2 + 1 - 2*g) - \
+            (2*g*(2*k1 + s + 1) - (k1 + k2)*(k1 + k2 + 1)) - (g**2 + s*(s + 1) + A)
+    gamma = lambda n: 2*g*(n + k1 + k2 + s)
+
+    a = np.zeros(n_max)
+    
+    # compute coefficients starting from a0 = 1 and normalize at the end
+    a[0] = 1
+    a[1] = -beta(0,A)/alpha(0)*a[0]
+
+    # if num_terms is specified, loop until that number of terms is reached
+    if num_terms is not None:
+        n_max = num_terms
+
+    norm = 0
+    for i in range(0,n_max):
+        n = i+1 # track number of terms that have been computed
+        # recursion relation for a_i
+        if i>1:
+            a[i] = -1/alpha(i-1)*(beta(i-1,A)*a[i-1]+gamma(i-1)*a[i-2])
+
+        # normterm comes from Integrate[Exp[2*g*(x - 1)]*x^(2*k1)*(2 - x)^(2*k2)*(c*x^i), {x, 0, 2}]
+        # c = \sum_0^i a_j*a_{i-j} is the coefficient of x^i in (\sum_0^i a_j x^j)^2 and x = 1+u = 1+cos(theta)
+        # terms that are independent of i have been factored out
+        normterm = 2**i*special.poch(i+2*(1+k1+k2), -2*k2-1)*special.hyp1f1(1+i+2*k1, i+2*(1+k1+k2), 4*g)*a[:i+1].dot(a[i::-1])
+
+        # break once machine precision is reached unless num_terms is specified
+        if (norm+normterm == norm) and (num_terms is None):
+            break
+        norm = norm + normterm
+
+    # multiply by the terms factored out earlier along with a factor of 2*pi from the integral over phi
+    norm = sqrt(2*pi*2**(1+2*k1+2*k2)*exp(-2*g)*special.gamma(1+2*k2)*norm)
+
+    # square of the theta component of Sslm written in terms of x = 1+u = 1+cos(theta)
+    # sslm2 = lambda x: np.exp(2*g*(x-1))*x**(2*k1)*(2-x)**(2*k2)*np.polynomial.Polynomial(a)(x)**2
+    # norm = sqrt(2*np.pi*scipy.integrate.quad(sslm2,0,2)[0])
+
+    return a[:n]/norm
+
+def harmonic_leaver(s,ell,m,g,num_terms=None):
+
+    k1 = 1/2*abs(m-s)
+    k2 = 1/2*abs(m+s)
+
+    a = leaver_coefficients(s,ell,m,g,num_terms)
 
     def Sslm(theta,phi):
-        spherical_harmonics = np.array([sphericalY(s,l,m)(theta,phi) for l in range(l_min,l_min+num_terms)])
-        return spherical_harmonics.dot(coefficients)
+        u = np.cos(theta)
+        basis = [(1+u)**n for n in range(len(a))]
+        return np.exp(g*u)*(1+u)**k1*(1-u)**k2*a.dot(basis)*np.exp(1j*m*phi)
     
     return Sslm
 
