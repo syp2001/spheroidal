d750587f0c1961e8b3d2210a483e5147b9b4ba30
Seyong <seyongpark2001@gmail.com>
2023-12-11 12:05:52 -0500

    typo fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 import setuptools
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="spheroidal",
-    version="0.0.1",
+    version="0.1.0",
     author="Seyong Park",
     description="Library for computing spin-weighted spheroidal harmonics",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/syp2001/spheroidal",
     packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=(
