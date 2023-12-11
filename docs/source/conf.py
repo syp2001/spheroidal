78ca7d76c8267349817ba1842f2d86732d3497e1
Seyong <seyongpark2001@gmail.com>
2023-12-11 12:05:52 -0500

    typo fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Seyong Park'
 author = 'Seyong Park'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
